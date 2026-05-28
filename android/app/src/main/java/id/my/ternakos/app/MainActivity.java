9ec1853d222b711b6cf527f4290a63f7b5506419
Fahru Hernan <fahruhernan@example.com>
2026-05-28 17:36:49 +0700

    feat: commit Android platform configs, enable WebView debugging, and fix workflow peer dependencies installation

---
+++ android/app/src/main/java/id/my/ternakos/app/MainActivity.java
@@ -0,0 +1,15 @@
+package id.my.ternakos.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable WebView debugging for Chrome DevTools (chrome://inspect)
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
